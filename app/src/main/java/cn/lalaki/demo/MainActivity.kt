22581786ee9354c1018c4d690aeb82387de5d13d
lalakii <i@lalaki.cn>
2026-02-15 21:02:22 +0800

    aes

--- app/src/main/java/cn/lalaki/demo/MainActivity.kt
+++ app/src/main/java/cn/lalaki/demo/MainActivity.kt
@@ -3,13 +3,20 @@ package cn.lalaki.demo
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Base64
 import android.widget.Button
+import android.widget.ScrollView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatEditText
 import cn.lalaki.demo19999.R
 import cn.lalaki.save.vars.SaveVar
+import java.io.File
+import java.nio.file.Files
 import java.nio.file.Paths
+import kotlin.io.path.isRegularFile
 import kotlin.system.exitProcess
 
 /**
@@ -27,20 +34,45 @@ class MainActivity : AppCompatActivity(), TextWatcher {
         setContentView(R.layout.main)
         textView = findViewById(R.id.textView)
         val editText = findViewById<AppCompatEditText>(R.id.editText)
+        val configPath = Paths.get(filesDir.canonicalPath, "config.ini")
         try {
-            SaveVar.init(Paths.get(filesDir.canonicalPath, "config.ini"))
+            SaveVar.init(configPath, Base64.decode("3pCTBQDsTgsHKetQmcFep+MAJo+27JpV9psxBAsdJro=",
+                Base64.DEFAULT))
             editText.append(SaveVar.INSTANCE.get("save"))
-        } catch (ignored: Throwable) {
+        } catch (_: Throwable) {
         }
         editText.addTextChangedListener(this)
         findViewById<Button>(R.id.btn_kill).setOnClickListener {
             finish()
             exitProcess(0)
         }
-        SaveVar.INSTANCE.set("key", "value")
-        SaveVar.INSTANCE.set("key", listOf("args0", "args1", "args2"), "&")
-        SaveVar.INSTANCE.get("key")
-        SaveVar.INSTANCE.get("key", "&")
+        findViewById<Button>(R.id.btn_rm_config).setOnClickListener {
+            if (configPath.isRegularFile()) {
+                Files.delete(configPath)
+                Toast.makeText(this@MainActivity, R.string.delete, Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+        findViewById<Button>(R.id.view_config).setOnClickListener {
+            if (configPath.isRegularFile()) {
+                var lines: List<String>? = null
+                try {
+                    lines = Files.readAllLines(configPath)
+                } catch (_: Throwable) {
+                }
+                if (lines != null) {
+                    val tv = TextView(this)
+                    for (line in lines) {
+                        tv.append("${line}\r\n")
+                    }
+                    AlertDialog.Builder(this).setView(ScrollView(this).apply { addView(tv) }).show()
+                }
+            }
+        }
+        //  SaveVar.INSTANCE.set("key", "value")
+        // SaveVar.INSTANCE.set("key", listOf("args0", "args1", "args2"), "&")
+        // SaveVar.INSTANCE.get("key")
+        // SaveVar.INSTANCE.get("key", "&")
     }
 
     override fun beforeTextChanged(
@@ -60,6 +92,6 @@ class MainActivity : AppCompatActivity(), TextWatcher {
     }
 
     override fun afterTextChanged(s: Editable?) {
-        SaveVar.INSTANCE.set("save", s.toString())
+        SaveVar.INSTANCE.set("save", "${s?.toString()}")
     }
 }
--- save_var/src/main/java/cn/lalaki/save/vars/SaveVar.kt
+++ save_var/src/main/java/cn/lalaki/save/vars/SaveVar.kt
@@ -1,8 +1,16 @@
 package cn.lalaki.save.vars
 
 import android.util.Base64
+import java.nio.file.Files
 import java.nio.file.Path
+import java.security.SecureRandom
 import java.util.Properties
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+import javax.crypto.spec.GCMParameterSpec
+import javax.crypto.spec.SecretKeySpec
+import kotlin.io.path.absolutePathString
 import kotlin.io.path.inputStream
 import kotlin.io.path.isRegularFile
 import kotlin.io.path.outputStream
@@ -10,12 +18,45 @@ import kotlin.io.path.outputStream
 @Suppress("MemberVisibilityCanBePrivate")
 open class SaveVar : Properties() {
     companion object {
-        fun init(config: Path?) {
+        @Synchronized
+        fun init(config: Path?, vararg secretKeyArray: ByteArray) {
             if (config != null) {
                 sCONFIG = config
             }
             val mConfig = sCONFIG
             if (mConfig != null && mConfig.isRegularFile()) {
+                val ivData = ByteArray(12)
+                SecureRandom().nextBytes(ivData)
+                val iv = GCMParameterSpec(96, ivData)
+                mIV = iv
+                val cipher = Cipher.getInstance("AES/GCM/NoPadding")
+                mCipher = cipher
+                var key: SecretKey? = null
+                if (secretKeyArray.isNotEmpty()) {
+                    key = SecretKeySpec(secretKeyArray[0], cipher.algorithm)
+                }
+                val localKey = Path.of(mConfig.parent.absolutePathString(), ".secret.key")
+                if (key == null && localKey.isRegularFile()) {
+                    var keyData: ByteArray? = null
+                    try {
+                        keyData = Files.readAllBytes(localKey)
+                    } catch (_: Throwable) {
+
+                    }
+                    if (keyData != null) {
+                        key = SecretKeySpec(keyData, cipher.algorithm)
+                    }
+                }
+                if (key == null) {
+                    val keyGen = KeyGenerator.getInstance(cipher.algorithm)
+                    keyGen.init(256)
+                    key = keyGen.generateKey()
+                    try {
+                        Files.write(localKey, key.encoded)
+                    } catch (_: Throwable) {
+                    }
+                }
+                mSecretKey = key
                 mConfig.inputStream().use {
                     INSTANCE.load(it)
                 }
@@ -23,27 +64,53 @@ open class SaveVar : Properties() {
         }
 
         var sCONFIG: Path? = null
+        var mCipher: Cipher? = null
+        var mIV: GCMParameterSpec? = null
+        var mSecretKey: SecretKey? = null
         val INSTANCE by lazy(LazyThreadSafetyMode.SYNCHRONIZED) { SaveVar() }
     }
 
+    @Synchronized
     fun get(name: String?): String {
         var value = ""
         if (!name.isNullOrEmpty()) {
             value = getProperty(name, "")
-            if (!value.isNullOrEmpty()) {
-                value = Base64.decode(value, Base64.DEFAULT).decodeToString()
+            if (value.isNotEmpty()) {
+                var decodeData = Base64.decode(value, Base64.NO_PADDING or Base64.NO_WRAP)
+                val cipher = mCipher
+                val key = mSecretKey
+                val iv = mIV
+                if (cipher != null && key != null && iv != null) {
+                    val ivSize = iv.iv.size
+                    val myIv = decodeData.copyOfRange(decodeData.size - ivSize, decodeData.size)
+                    cipher.init(Cipher.DECRYPT_MODE, key, GCMParameterSpec(iv.tLen, myIv))
+                    decodeData = cipher.doFinal(decodeData.dropLast(ivSize).toByteArray())
+                }
+                value = decodeData.decodeToString()
             }
         }
         return value
     }
 
+    @Synchronized
     fun set(
         name: String,
         value: String?,
     ) {
         if (name.isNotEmpty()) {
             if (!value.isNullOrEmpty()) {
-                setProperty(name, Base64.encodeToString(value.encodeToByteArray(), Base64.DEFAULT))
+                val cipher = mCipher
+                val key = mSecretKey
+                val iv = mIV
+                var data = value.encodeToByteArray()
+                if (cipher != null && key != null && iv != null) {
+                    cipher.init(Cipher.ENCRYPT_MODE, key, iv)
+                    data = cipher.doFinal(data)
+                    data += iv.iv
+                }
+                setProperty(
+                    name, Base64.encodeToString(data, Base64.NO_PADDING or Base64.NO_WRAP)
+                )
             } else {
                 unset(name)
             }
@@ -53,10 +120,7 @@ open class SaveVar : Properties() {
         }
     }
 
-    private fun unset(name: String) {
-        this.setProperty(name, "")
-    }
-
+    @Synchronized
     fun set(
         name: String,
         value: List<String>?,
@@ -69,6 +133,7 @@ open class SaveVar : Properties() {
         }
     }
 
+    @Synchronized
     fun get(
         name: String?,
         separator: String,
@@ -78,5 +143,9 @@ open class SaveVar : Properties() {
             return value.split(separator)
         }
         return null
+    }
+
+    private fun unset(name: String) {
+        this.setProperty(name, "")
     }
 }
